68073436722fe307ed5c86bac404384f50288e8f
ll <leslieengr@gmail.com>
2023-10-25 14:30:17 +0700

    OK

--- hello/app.py
+++ hello/app.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template, request
 #thong tin ve cac yeu cau HTTP gui den may chu, render: module tao va render cac trang Http or mau.
 app = Flask(__name__)
 
-@app.route("/")
+registrants = {}
+
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("index.html")
 
+    return render_template("index.html")  
 
-@app.route("/greet")
-def greet():
-    return render_template("greet.html", name=request.args.get("name","world"))
+@app.route("/registrants", methods=["POST"])
+def registrants():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    registrants[name] = sport
+    return render_template("success.html")
